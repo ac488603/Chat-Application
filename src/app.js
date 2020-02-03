487c10e94f224003f54e58d8a00ea82c31fc3a8e
Adam Castillo <ac488603@mail.fresnostate.edu>
2020-02-03 11:29:58 -0800

    installed socket.io library. refactored code to get socket.io to work on server. added initial socket.io script tags to index.html

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 const express = require('express')
+const socketio = require('socket.io')
+const http = require('http')
+
 const app = express()
+const server = http.createServer(app)
+const io = socketio(server)
+
+io.on('connection', () => console.log('New Socket Connected'))
 
 app.use(express.static('public'))
 
 
-module.exports = app;
+module.exports = server;
