e2783c8b8509b86a47bd997e48083edba7347fa7
Adam Castillo <ac488603@mail.fresnostate.edu>
2020-02-09 20:27:11 -0800

    added ability to click location link.

--- public/js/chat.js
+++ public/js/chat.js
@@ -2,47 +2,47 @@ const socket = io();
 const chat = document.getElementById('chatfeed')
 const input = document.getElementById('input')
 const form = document.getElementById('form')
-const sendLoc = document.getElementById('sl')
-//redners message to screen
+const sendLoc = document.getElementById('send-location')
+
+//renders message to screen
 socket.on('update', (message) => {
-    if(message!== undefined){
+    if (message !== undefined) {
         let li = document.createElement('li')
         li.innerHTML = message
         chat.appendChild(li)
     }
 })
 
+socket.on('showLocation', (link) => {
+    let anchor = document.createElement('a')
+    anchor.setAttribute('href', link)
+    anchor.innerHTML = 'view location'
+    chat.appendChild(anchor)
+})
 
 //waits for user to push submit
 //when user hit submit the message is sent to all clients
-form.addEventListener('submit',(e) => {
+form.addEventListener('submit', (e) => {
     e.preventDefault();
-    socket.emit('sendMessage', input.value)
+
+    if(input.value !== '')
+        socket.emit('sendMessage', input.value)
 })
 
 
-document.querySelector('#send-location').addEventListener('click',()=>{
-    if(!navigator.geolocation)
+document.querySelector('#send-location').addEventListener('click', () => {
+    if (!navigator.geolocation)
         return alert("Geolocation is not supported by your browser")
-    navigator.geolocation.getCurrentPosition( position =>{
-        socket.emit('sendLocation', {lat:position.coords.latitude, long:position.coords.longitude}, _ => {
-            console.log('Location Received.')
+    sendLoc.setAttribute('disabled', 'disabled')
+    navigator.geolocation.getCurrentPosition(position => {
+        socket.emit('sendLocation', {
+            lat: position.coords.latitude,
+            long: position.coords.longitude
+        }, _ => {
+            console.log('Location Received.') //acknowledgement 
+            sendLoc.removeAttribute('disabled')
         })
     }, error => {
         console.log(error)
     })
 })
-
-
-// sendLoc.addEventListener('click', () => {
-//     if(!navigator.geolocation){
-//         return alert('Geolocation is not supported by your browser.')
-//     }
-
-//     navigator.geolocation.getCurrentPosition( position => {
-//         console.log(position)
-//     }, error => {
-//         console.log(error)
-//     })
-    
-// })
--- src/app.js
+++ src/app.js
@@ -8,17 +8,18 @@ const io = socketio(server)
 
 io.on('connection', (socket) => {
     socket.emit('update', 'Welcome to the Chat!')
-    socket.broadcast.emit('update',  'A new user has joined!')
+    socket.broadcast.emit('update', 'A new user has joined!')
     socket.on('sendMessage', (message) => {
         io.emit('update', message)
     })
     socket.on('disconnect', () => {
         io.emit('update', 'A user has left.')
     })
+    //the callback is used for acknowledgements
     socket.on('sendLocation', (position, callback) => {
-           const mapLink = `https://google.com/maps?q=${position.lat},${position.long}`
-           callback()
-        io.emit('update',mapLink )
+        const mapLink = `https://google.com/maps?q=${position.lat},${position.long}`
+        callback()
+        io.emit('showLocation', mapLink)
     })
 })
 
