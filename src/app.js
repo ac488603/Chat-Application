212f6339b9b091e5a1ec2064e9cc16204101f87f
Adam Castillo <ac488603@mail.fresnostate.edu>
2020-02-10 00:58:51 -0800

    added favicon and an icon.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const socketio = require('socket.io')
 const http = require('http')
-const im =  require('./utils/message')
+const im = require('./utils/message')
 
 const app = express()
 const server = http.createServer(app)
