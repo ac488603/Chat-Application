10cd2069055eb7809049e74d1956a15e452c923f
Adam Castillo <ac488603@mail.fresnostate.edu>
2020-02-12 11:14:03 -0800

    exported user utility functions.

--- src/utils/users.js
+++ src/utils/users.js
@@ -1,49 +1,67 @@
 const users = []
 
-const addUsers = ({id,username,room}) => {
+const addUsers = ({
+    id,
+    username,
+    room
+}) => {
     //sanitze data 
     username = username.trim().toLowerCase()
     room = room.trim().toLowerCase()
 
     //validate data
-    if(!username || !room){
+    if (!username || !room) {
         return {
-            error : "username and room are requried."
+            error: "username and room are requried."
         }
     }
 
     //check if username is unique
 
     const exists = users.find(user => {
-        return user.room === room && user.username ==username
+        return user.room === room && user.username == username
     })
 
-    if(exists){
+    if (exists) {
         return {
             error: "username is not unique"
         }
     }
 
-    const user = {username, room, id}
+    const user = {
+        username,
+        room,
+        id
+    }
     users.push(user)
-    return {user}
+    return {
+        user
+    }
 }
 
 const removeUsers = (id) => {
-    const index  = users.findIndex((user) => {
+    const index = users.findIndex((user) => {
         return user.id == id
     })
-    if(index !== -1){
+    if (index !== -1) {
         return users.splice(index, 1)[0]
     }
 }
 
 const getUser = (id) => {
-    return users.find( user => {
+    return users.find(user => {
         return user.id = id
     })
 }
 
 const getUsersInRoom = (room) => {
-    return users.filter( user => user.room == room) 
+    return users.filter(user => user.room == room)
+}
+
+
+module.exports = {
+    getUser,
+    getUsersInRoom,
+    removeUsers,
+    addUsers
 }
